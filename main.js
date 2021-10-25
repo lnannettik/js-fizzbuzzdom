9ae75db2908717523dee4ae2b237892a359a005b
lnannettik <l.nannetti.k@gmail.com>
2021-10-25 16:13:53 +0200

    problema box

--- main.js
+++ main.js
@@ -3,14 +3,28 @@
 console.log(`JS OK`);
 
 
-// ref container boxes
+// ref
 const boxes = document.querySelector(`.boxes`);
+
+const box = document.querySelector(`.box`);
+
+const green = document.querySelector(`.green`);
+
+const yellow = document.querySelector(`.yellow`);
+
+const red = document.querySelector(`.red`);
+
 
 // generare i box
 for (let i = 1 ; i <= 100 ; i++) {
     console.log(`funziona ${i}`)
 
     boxes.innerHTML += `<div class="box box-${i}">${i}</div>`;
+
+    if (i % 3 == 0) {
+        box.classList.add(`green`);
+    }
+
 
 }
 
