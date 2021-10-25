7bffe4a0976ba771a91d92b2d9c6893463e229e9
lnannettik <l.nannetti.k@gmail.com>
2021-10-25 17:27:18 +0200

    finito

--- main.js
+++ main.js
@@ -6,25 +6,29 @@ console.log(`JS OK`);
 // ref
 const boxes = document.querySelector(`.boxes`);
 
-const box = document.querySelector(`.box`);
+// const green = document.querySelector(`.green`);
 
-const green = document.querySelector(`.green`);
+// const yellow = document.querySelector(`.yellow`);
 
-const yellow = document.querySelector(`.yellow`);
-
-const red = document.querySelector(`.red`);
+// const red = document.querySelector(`.red`);
 
 
 // generare i box
 for (let i = 1 ; i <= 100 ; i++) {
-    console.log(`funziona ${i}`)
+    // console.log(`${i}`)
 
-    boxes.innerHTML += `<div class="box box-${i}">${i}</div>`;
-
-    if (i % 3 == 0) {
-        box.classList.add(`green`);
+    if ((i % 5 == 0) && (i % 3 == 0)) {
+        boxes.innerHTML += `<div class="box red box-${i}">FizzBuzz</div>`;
+        console.log(`FizzBuzz`)
+    } else if (i % 3 == 0) {
+        boxes.innerHTML += `<div class="box green box-${i}">Fizz</div>`;
+        console.log(`fizz`)
+    } else if (i % 5 == 0) {
+        boxes.innerHTML += `<div class="box yellow box-${i}">Buzz</div>`;
+        console.log(`buzz`)
+    } else {
+        boxes.innerHTML += `<div class="box normal box-${i}">${i}</div>`;
     }
-
 
 }
 
